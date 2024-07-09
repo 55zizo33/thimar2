cf6e1680fc80eba13af5327a0311378883048f5b
Ahmed Sobhy <55zizo33@gmail.com>
2024-07-09 19:41:08 +0300

    first commit

--- lib/core/cmd.dart
+++ lib/core/cmd.dart
@@ -3,6 +3,6 @@ git init
 git add .
 git commit -m "first commit"
 git branch -M main
-git remote add origin https://github.com/amramer522/movies_map_amit63.git
+git remote add origin https://github.com/55zizo33/thimar2
 git push -u origin main
  */
